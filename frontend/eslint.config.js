88970f4366b61ce71fdea9d0bc701d596060b208
shima maleki <shimamaleki95@yahoo.com>
2026-06-02 00:31:06 +0100

    adding .gitignore file

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,9 +2,11 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
 
 export default [
   { ignores: ["dist"] },
+  ...tseslint.configs.recommended,
   {
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
@@ -23,7 +25,8 @@ export default [
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }]
+      "no-undef": "off",
+      "react-refresh/only-export-components": "off"
     }
   }
 ];
